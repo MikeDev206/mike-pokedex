9bf51f2b43ab776db1edc400c9912284c4feafcd
MikeDev206 <acevesm77@gmail.com>
2024-11-06 15:01:05 -0600

    rutas, y controlaores agregados

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();//esto es para cargar las variables de entorno
 const express = require('express');
 const connectDB = require('./config/db');
+const trainerRoutes = require('./routes/trainerRoutes');
 
 const app = express();
 
@@ -11,10 +12,13 @@ connectDB();
 //iniciar el middleware
 app.use(express.json());
 
-//definir la ruta
+//definir la ruta de prueba
 app.get('/', (req, res) => {
 	res.send('Server funcionando :P, yeeih!');
 });
+
+//ruta de los entrenadores
+app.use('/api/trainers', trainerRoutes);
 
 
 const PORT = process.env.PORT || 5050;
