9a146d141fb1df32dea2922476143991920d947e
MikeDev206 <acevesm77@gmail.com>
2024-11-07 12:44:14 -0600

    refactorización del código dejando más claridad en las funciones y separa la función fetchPokemons para manejar la obtención, filtrado y orden de pokemons

--- backend/routes/pokemonRoutes.js
+++ backend/routes/pokemonRoutes.js
@@ -2,33 +2,32 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+const fetchPokemons = async (limit, offset, search) => {
+	const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+	let pokemons = response.data.results;
+
+	if (search) {
+		pokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
+	}
+	return pokemons.sort((a, b) => a.name.localeCompare(b.name));
+}
+
 //ruta para obtener todos los Pokémon con paginación y búsqueda
-router.get('/', async (req, res) => {
+router.get('/pokemons', async (req, res) => {
 	const { limit = 10, page = 1, search } = req.query;
+	const offset = (page - 1) * limit;
 
 	//valida que limit y page sean números
-	if (isNaN(limit) || isNaN(page) || limit <= 0 || page <= 0) {
+	if (Number.isNaN(Number(limit)) || Number.isNaN(Number(page)) || limit <= 0 || page <= 0) {
 		return res.status(400).json({ error: 'Parámetro inválido' });
 	}
 
-	const offset = (page - 1) * limit;
-
 	try {
-		const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-		let pokemons = response.data.results;
-
-		//filtra por nombre si se proporciona el parámetro search
-		if (search) {
-			pokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
-		}
-
-		//ordena alfabéticamente
-		pokemons.sort((a, b) => a.name.localeCompare(b.name));
-
+		const pokemons = await fetchPokemons(limit, offset, search);
 		res.json(pokemons);
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ error: 'Error fetching data from PokeAPI' });
+		res.status(500).json({ error: 'Error al obtener datos de la PokeAPI 🤔' });
 	}
 });
 
