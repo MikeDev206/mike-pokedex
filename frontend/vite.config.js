e4709f87165a60739dc519aeef92f1739d8f451c
MikeDev206 <acevesm77@gmail.com>
2024-11-07 20:21:35 -0600

    build terminado, vite.config modificado para que apunte a carpeta raiz

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
+  root: './frontend',
+  base: '/mike-pokedex/',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     rollupOptions: {
-      input: './index.html',
+      input: './frontend/index.html',
     },
   },
   server: {
