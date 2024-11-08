a4476793d8c2e71a23c7e3274da0ad7df6c8d2ad
MikeDev206 <acevesm77@gmail.com>
2024-11-07 18:34:51 -0600

    cors y react-dom warning corregidos

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const trainerRoutes = require('./routes/trainerRoutes');
 const pokemonRoutes = require('./routes/pokemonRoutes');
 
